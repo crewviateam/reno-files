1dca038d3978ca2b9c91d5ce8d12830ed22f58c9
husain-amjhera14 <husainamjhera5253@gmail.com>
2026-03-21 01:03:35 +0530

    Fixing imagine wrapper appearence

--- public/app.js
+++ public/app.js
@@ -71,7 +71,7 @@ document.addEventListener("DOMContentLoaded", () => {
         scrollTrigger: {
           id: "tabPaneTrigger_main",
           trigger: triggerEl,
-          start: "top top",
+          start: "top 120%",
           end: "bottom bottom",
           scrub: 0.5,
           invalidateOnRefresh: true,
