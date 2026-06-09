ae6422257de5b513157a457e602bb9f7ed984dda
crewviateam <crewviateam@gmail.com>
2026-06-09 06:25:28 +0530

    Update home.js

--- public/home.js
+++ public/home.js
@@ -439,8 +439,8 @@ function GallerySecAnimation() {
     const yValue2 = isMobile ? "-71.9715%" : "-54.9715%";
 
     galTl.to(animImage, {
-      width: "101dvw",
-      height: "110dvh",
+      width: "100vw",
+      height: "110vh",
       y: yValue2,
       duration: 0.5,
       transformOrigin: "100% 100%",
